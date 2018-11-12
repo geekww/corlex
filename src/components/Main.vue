5aabfed579a9fa13e62cc8b8ac18a611b1f9d47b
王伟 <solexit28@solex.com>
2018-11-12 17:19:52 +0800

    add

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container>
+    <SideBar></SideBar>
+    <Header></Header>
+  </el-container>
+</template>
+
+<script>
+  import Header from './Header.vue'
+  import SideBar from './SideBar.vue'
+
+  export default {
+    components:{Header,SideBar},
+    name: 'main'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-aside>
+    <el-menu :default-openeds="['1']">
+      <el-submenu index="1">
+        <template slot="title"><i class="el-icon-message"></i>导航一</template>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title"><i class="el-icon-message"></i>导航二</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-aside {
+    text-align: center;
+    line-height: 200px;
+    position: absolute;
+    top: 60px;
+  }
+  .el-submenu{
+
+  }
+  .el-submenu__title{
+  }
+  .el-submenu__title i{
+  }
+</style>
