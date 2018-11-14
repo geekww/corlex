930db950e22aaabf968479a5a27c878732a9ffc1
wangwei <jike_wangwei@163.com>
2018-11-14 22:59:31 +0800

    创建项目表单

--- src/components/project/CreateProject.vue
+++ src/components/project/CreateProject.vue
@@ -1,47 +1,48 @@
 <template>
-  <div class="form-box">
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="项目名称">
+  <div class="module-box">
+    <el-form ref="form" :model="form" label-width="100px">
+      <el-form-item label="项目编号：">
+        <el-input v-model="form.pid"></el-input>
+      </el-form-item>
+      <el-form-item label="项目名称：">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+      <el-form-item label="处理优先级：">
+        <el-select v-model="form.level" placeholder="请选择项目优先级别">
+          <el-option label="紧急" value="0"></el-option>
+          <el-option label="高" value="1"></el-option>
+          <el-option label="中" value="2"></el-option>
+          <el-option label="低" value="3"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+      <el-form-item label="创建时间：">
+        <el-col>
+          <el-date-picker type="date" placeholder="选择日期" v-model="form.date" style="width: 100%;"></el-date-picker>
         </el-col>
       </el-form-item>
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式">
+      <!--<el-form-item label="即时配送">-->
+      <!--<el-switch v-model="form.delivery"></el-switch>-->
+      <!--</el-form-item>-->
+      <!--<el-form-item label="活动性质">-->
+      <!--<el-checkbox-group v-model="form.type">-->
+      <!--<el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>-->
+      <!--<el-checkbox label="地推活动" name="type"></el-checkbox>-->
+      <!--<el-checkbox label="线下主题活动" name="type"></el-checkbox>-->
+      <!--<el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>-->
+      <!--</el-checkbox-group>-->
+      <!--</el-form-item>-->
+      <!--<el-form-item label="特殊资源">-->
+      <!--<el-radio-group v-model="form.resource">-->
+      <!--<el-radio label="线上品牌商赞助"></el-radio>-->
+      <!--<el-radio label="线下场地免费"></el-radio>-->
+      <!--</el-radio-group>-->
+      <!--</el-form-item>-->
+      <el-form-item label="项目描述：">
         <el-input type="textarea" v-model="form.desc"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
+        <!--<el-button>取消</el-button>-->
       </el-form-item>
     </el-form>
   </div>
@@ -53,37 +54,53 @@
     data() {
       return {
         form: {
+          pid: '',
           name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
+          level: '',
+          date: '',
           desc: ''
         }
       }
     },
     methods: {
       onSubmit() {
-        this.$message('创建成功');
+        if(!this.form.name){
+          this.$message('请输入项目名');
+          return false;
+        }
+        this.$message(this.form.name);
       }
     }
   }
 </script>
 
 <style scoped>
-  .form-box{
-    width: 800px;
+  .module-box{
+    width: 400px;
     background: #fff;
     padding: 50px 60px;
     box-shadow:0 5px 20px #E4E8EE;
     border-radius:12px;
     animation: fadein .4s;
+    margin: 10px auto;
   }
   @keyframes fadein
   {
-    from {margin-top:-30px;opacity: 0}
-    to {margin-top:0;opacity: 1}
+    from {opacity: 0}
+    to {opacity: 1}
+  }
+  .el-input{
+    width: 200px;
+    float: left;
+  }
+  .el-select{
+    float: left;
+    width: 200px;
+  }
+  .el-col{
+    width: 200px;
+  }
+  .el-button{
+    float: left;
   }
 </style>
--- src/components/project/RepairProject.vue
+++ src/components/project/RepairProject.vue
@@ -1,13 +1,74 @@
 <template>
+  <div class="module-box">
+    <el-table :data="view" style="width: 100%">
+      <el-table-column label="项目编号" prop="pid"></el-table-column>
+      <el-table-column label="项目名称" prop="name"></el-table-column>
+      <el-table-column label="描述" prop="desc"></el-table-column>
 
+      <el-table-column type="expand">
+        <template slot-scope="props">
+          <el-form label-position="left" inline class="demo-table-expand">
+            <el-form-item label="项目名称：">
+              <span>{{ props.row.name }}</span>
+            </el-form-item>
+            <el-form-item label="负责人：">
+              <span>{{ props.row.fzr }}</span>
+            </el-form-item>
+            <el-form-item label="创建日期：">
+              <span>{{ props.row.date }}</span>
+            </el-form-item>
+            <el-form-item label="描述：">
+              <span>{{ props.row.desc }}</span>
+            </el-form-item>
+          </el-form>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'RepairProject'
+    name: 'RepairProject',
+    data() {
+      return {
+        view: [{
+          pid: '100001',
+          name: '松霖家居网上商城',
+          fzr: 'corlex',
+          date: '2018-11-17',
+          desc: '松霖家居网上商城',
+        }]
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  .module-box{
+    width: 700px;
+    background: #fff;
+    padding: 30px 60px;
+    box-shadow:0 5px 20px #E4E8EE;
+    border-radius:12px;
+    animation: fadein .4s;
+    margin: 10px auto;
+  }
+  @keyframes fadein
+  {
+    from {opacity: 0}
+    to {opacity: 1}
+  }
+  .demo-table-expand {
+    font-size: 0;
+  }
+  .demo-table-expand label {
+    width: 90px;
+    color: #99a9bf;
+  }
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 50%;
+  }
 </style>
