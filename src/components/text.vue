ef4c91d29fcbe7d1330067fca5e03f7887c2f22e
wangwei <jike_wangwei@163.com>
2018-12-14 17:15:00 +0800

    add

---
+++ src/components/text.vue
@@ -0,0 +1,24 @@
+<template>
+  <!--变量绑定-->
+  <h2>{{title}}</h2>
+  <!--双向绑定-->
+  <input v-model="name">
+  <h1>{{name}}</h1>
+  <!--事件绑定-->
+  <button v-on:click="clickMe()">事件绑定</button>
+</template>
+
+<script>
+  export default {
+    name: 'model',
+    data: {
+      title: 'hello world',
+      name: 'canfoo'
+    },
+    methods: {
+      clickMe: function () {
+        this.title = 'hello world';
+      }
+    }
+  }
+</script>
